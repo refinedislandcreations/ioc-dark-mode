f2598b103dd968a98046741e4906aa58f52424d1
JuliaRees <julia@refinedislandcreations.com>
2024-11-10 10:03:15 +0800

    save

--- scripts.js
+++
@@ -1,7 +0,0 @@
-$('.toggle-container').click(function(){
-    console.log("click")
-    $('body').toggleClass('dark');
-    $(this).toggleClass('dark-mode-active')
-  });
-
-  console.log("jsloaded")
